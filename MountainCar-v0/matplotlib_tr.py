ac3c4a4f9a4897f792887d93cde29c9a0f4cedd5
Kerneltyu <kerneltyu@gmail.com>
2018-05-13 21:24:59 +0900

    first commit

---
+++ MountainCar-v0/matplotlib_tr.py
@@ -0,0 +1,20 @@
+import math
+import numpy as np
+from matplotlib import pyplot
+
+pi = math.pi
+
+x = np.linspace(0, 2*pi, 100)
+sin_y = np.sin(x)
+cos_y = np.cos(x)
+
+pyplot.plot(x, sin_y)
+pyplot.plot(x, cos_y)
+
+pyplot.title('Sin and Cos Graph')
+
+pyplot.xlabel('X-Axis')
+pyplot.ylabel('Y-Axis')
+
+pyplot.legend()
+pyplot.show()
